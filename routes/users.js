1f86bbb8402a3d9e19bdc2f7b3e3942353610be1
Jennifer Winer <jeniwiner@gmail.com>
2017-03-29 16:33:53 -0600

    successful post route for /users with hashed password

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,26 @@ const express = require('express');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
+const knex = require('../knex');
+const bcrypt = require('bcrypt-as-promised');
+const humps = require('humps')
 
 // YOUR CODE HERE
+router.post('/users', (req, res, next) => {
+  bcrypt.hash(req.body.password, 12)
+  .then((hashed_password) => {
+    return knex('users')
+    .insert({
+      first_name: req.body.firstName,
+      last_name: req.body.lastName,
+      email: req.body.email,
+      hashed_password: hashed_password
+    })
+    .returning(['id', 'first_name', 'last_name', 'email']);
+  })
+  .then(users => {
+    res.send(humps.camelizeKeys(users[0]));
+  })
+})
 
 module.exports = router;
